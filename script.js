1c4b39ba704a9f50870949142f9fd06b5412c3bd
1Cavalier <101728512+1Cavalier@users.noreply.github.com>
2022-03-19 18:09:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,187 @@
+function Coef_1()
+{
+    /* Partie Voictoire Normal */
+    document.getElementById("Vn_1").textContent=6;
+    document.getElementById("Vn_2").textContent=5.5;
+    document.getElementById("Vn_3").textContent=5;
+    document.getElementById("Vn_4").textContent=4;
+    document.getElementById("Vn_5").textContent=3;
+    document.getElementById("Vn_6").textContent=2;
+    document.getElementById("Vn_7").textContent=1;
+    document.getElementById("Vn_8").textContent=0.5;
+    document.getElementById("Vn_9").textContent=0;
+
+    /* Partie Défaite Normal */
+    document.getElementById("Dn_1").textContent=-5;
+    document.getElementById("Dn_2").textContent=-4.5;
+    document.getElementById("Dn_3").textContent=-4;
+    document.getElementById("Dn_4").textContent=-3;
+    document.getElementById("Dn_5").textContent=-2;
+    document.getElementById("Dn_6").textContent=-1;
+    document.getElementById("Dn_7").textContent=-0.5;
+    document.getElementById("Dn_8").textContent=0;
+    document.getElementById("Dn_9").textContent=0;
+
+    /* Partie Voictoire Normal */
+    document.getElementById("VA_1").textContent=6;
+    document.getElementById("VA_2").textContent=7;
+    document.getElementById("VA_3").textContent=8;
+    document.getElementById("VA_4").textContent=10;
+    document.getElementById("VA_5").textContent=13;
+    document.getElementById("VA_6").textContent=17;
+    document.getElementById("VA_7").textContent=22;
+    document.getElementById("VA_8").textContent=28;
+    document.getElementById("VA_9").textContent=40;
+
+    /* Partie Défaite Normal */
+    document.getElementById("DA_1").textContent=-5;
+    document.getElementById("DA_2").textContent=-6;
+    document.getElementById("DA_3").textContent=-7;
+    document.getElementById("DA_4").textContent=-8;
+    document.getElementById("DA_5").textContent=-10;
+    document.getElementById("DA_6").textContent=-12.5;
+    document.getElementById("DA_7").textContent=-16;
+    document.getElementById("DA_8").textContent=-20;
+    document.getElementById("DA_9").textContent=-28;
+}
+
+function Coef_125()
+{
+    /* Partie Voictoire Normal */
+    document.getElementById("Vn_1").textContent=7.5;
+    document.getElementById("Vn_2").textContent=6.875;
+    document.getElementById("Vn_3").textContent=6.25;
+    document.getElementById("Vn_4").textContent=5;
+    document.getElementById("Vn_5").textContent=3.75;
+    document.getElementById("Vn_6").textContent=2.5;
+    document.getElementById("Vn_7").textContent=1.25;
+    document.getElementById("Vn_8").textContent=0.625;
+    document.getElementById("Vn_9").textContent=0;
+
+    /* Partie Défaite Normal */
+    document.getElementById("Dn_1").textContent=-6.25;
+    document.getElementById("Dn_2").textContent=-5.625;
+    document.getElementById("Dn_3").textContent=-5;
+    document.getElementById("Dn_4").textContent=-3.75;
+    document.getElementById("Dn_5").textContent=-2.5;
+    document.getElementById("Dn_6").textContent=-1.25;
+    document.getElementById("Dn_7").textContent=-0.625;
+    document.getElementById("Dn_8").textContent=0;
+    document.getElementById("Dn_9").textContent=0;
+
+    /* Partie Voictoire Normal */
+    document.getElementById("VA_1").textContent=7.5;
+    document.getElementById("VA_2").textContent=8.75;
+    document.getElementById("VA_3").textContent=10;
+    document.getElementById("VA_4").textContent=12.5;
+    document.getElementById("VA_5").textContent=16.25;
+    document.getElementById("VA_6").textContent=21.25;
+    document.getElementById("VA_7").textContent=27.5;
+    document.getElementById("VA_8").textContent=35;
+    document.getElementById("VA_9").textContent=50;
+
+    /* Partie Défaite Normal */
+    document.getElementById("DA_1").textContent=-6.25;
+    document.getElementById("DA_2").textContent=-7.5;
+    document.getElementById("DA_3").textContent=-8.75;
+    document.getElementById("DA_4").textContent=-10;
+    document.getElementById("DA_5").textContent=-12.5;
+    document.getElementById("DA_6").textContent=-15.625;
+    document.getElementById("DA_7").textContent=-20;
+    document.getElementById("DA_8").textContent=-25;
+    document.getElementById("DA_9").textContent=-36.25;
+}
+
+function Coef_075()
+{
+    /* Partie Voictoire Normal */
+    document.getElementById("Vn_1").textContent=4.5;
+    document.getElementById("Vn_2").textContent=4.125;
+    document.getElementById("Vn_3").textContent=3.75;
+    document.getElementById("Vn_4").textContent=3;
+    document.getElementById("Vn_5").textContent=2.25;
+    document.getElementById("Vn_6").textContent=1.5;
+    document.getElementById("Vn_7").textContent=0.75;
+    document.getElementById("Vn_8").textContent=0.375;
+    document.getElementById("Vn_9").textContent=0;
+
+    /* Partie Défaite Normal */
+    document.getElementById("Dn_1").textContent=-3.75;
+    document.getElementById("Dn_2").textContent=-3.375;
+    document.getElementById("Dn_3").textContent=-3;
+    document.getElementById("Dn_4").textContent=-2.25;
+    document.getElementById("Dn_5").textContent=-1.5;
+    document.getElementById("Dn_6").textContent=-0.75;
+    document.getElementById("Dn_7").textContent=-0.375;
+    document.getElementById("Dn_8").textContent=0;
+    document.getElementById("Dn_9").textContent=0;
+
+    /* Partie Voictoire Normal */
+    document.getElementById("VA_1").textContent=4.5;
+    document.getElementById("VA_2").textContent=5.25;
+    document.getElementById("VA_3").textContent=6;
+    document.getElementById("VA_4").textContent=7.5;
+    document.getElementById("VA_5").textContent=9.75;
+    document.getElementById("VA_6").textContent=12.75;
+    document.getElementById("VA_7").textContent=16.5;
+    document.getElementById("VA_8").textContent=21;
+    document.getElementById("VA_9").textContent=30;
+
+    /* Partie Défaite Normal */
+    document.getElementById("DA_1").textContent=-3.75;
+    document.getElementById("DA_2").textContent=-4.5;
+    document.getElementById("DA_3").textContent=-5.25;
+    document.getElementById("DA_4").textContent=-6;
+    document.getElementById("DA_5").textContent=-7.5;
+    document.getElementById("DA_6").textContent=-9.375;
+    document.getElementById("DA_7").textContent=-12;
+    document.getElementById("DA_8").textContent=-15;
+    document.getElementById("DA_9").textContent=-21.75;
+}
+
+function Coef_05()
+{
+    /* Partie Voictoire Normal */
+    document.getElementById("Vn_1").textContent=3;
+    document.getElementById("Vn_2").textContent=2.75;
+    document.getElementById("Vn_3").textContent=2.5;
+    document.getElementById("Vn_4").textContent=2;
+    document.getElementById("Vn_5").textContent=1.5;
+    document.getElementById("Vn_6").textContent=1;
+    document.getElementById("Vn_7").textContent=0.5;
+    document.getElementById("Vn_8").textContent=0.25;
+    document.getElementById("Vn_9").textContent=0;
+
+    /* Partie Défaite Normal */
+    document.getElementById("Dn_1").textContent=-2.5;
+    document.getElementById("Dn_2").textContent=-2.25;
+    document.getElementById("Dn_3").textContent=-2;
+    document.getElementById("Dn_4").textContent=-1.5;
+    document.getElementById("Dn_5").textContent=-1;
+    document.getElementById("Dn_6").textContent=-0.5;
+    document.getElementById("Dn_7").textContent=-0.25;
+    document.getElementById("Dn_8").textContent=0;
+    document.getElementById("Dn_9").textContent=0;
+
+    /* Partie Voictoire Normal */
+    document.getElementById("VA_1").textContent=3;
+    document.getElementById("VA_2").textContent=3.5;
+    document.getElementById("VA_3").textContent=4;
+    document.getElementById("VA_4").textContent=5;
+    document.getElementById("VA_5").textContent=6.5;
+    document.getElementById("VA_6").textContent=8.5;
+    document.getElementById("VA_7").textContent=11;
+    document.getElementById("VA_8").textContent=14;
+    document.getElementById("VA_9").textContent=20;
+
+    /* Partie Défaite Normal */
+    document.getElementById("DA_1").textContent=-2.5;
+    document.getElementById("DA_2").textContent=-3;
+    document.getElementById("DA_3").textContent=-3.5;
+    document.getElementById("DA_4").textContent=-4;
+    document.getElementById("DA_5").textContent=-5;
+    document.getElementById("DA_6").textContent=-6.25;
+    document.getElementById("DA_7").textContent=-8;
+    document.getElementById("DA_8").textContent=-10;
+    document.getElementById("DA_9").textContent=-14.5;
+}
